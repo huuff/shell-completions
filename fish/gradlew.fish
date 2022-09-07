16be5857fc13407a94f56f4a6f05ffd883c41c28
Haf <haf@protonmail.ch>
2022-09-07 15:35:09 +0200

    considerable better fish completion script

--- fish/gradlew.fish
+++ fish/gradlew.fish
@@ -4,12 +4,12 @@ function _gradlew_complete
   set current_dir (basename "$PWD")
   set cache_file "/tmp/completions/gradlew/$current_dir"
 
-  if find "$cache_file" -newer ./build.gradle.kts -print
-    cat "$cache_file"
+  if find "$cache_file" -newer ./build.gradle.kts 2&> /dev/null
+    cat "$cache_file" | tr ' ' '\n'
   else
     mkdir -p $(dirname "$cache_file")
-    set tasks (./gradlew tasks | grep -Po "\w+(?=\W+-\.*)" | tr '\n' ' ')
-    echo "$tasks" | tee "$cache_file"
+    set tasks (./gradlew tasks | grep -Po "\w+(?=\W+-\.*)")
+    echo "$tasks" | tee "$cache_file" | tr ' ' '\n'
   end
 end
 
